458821552fe40e2ae4ff87eb91fc9873b05fa5f5
Yuxin Xie <dawndawn@ipsec-10-2-0-8.vpn.gatech.edu>
2022-09-13 12:41:05 -0400

    add steps for t1

--- get_command_for_router.py
+++ get_command_for_router.py
@@ -1,6 +1,7 @@
 # python get_command_for_router.py 1 # print commands for configuring ZURI
 
 import sys
+from collections import defaultdict
 
 GROUP_ID = '68'
 routers = ['','ZURI','BASE','GENE','LUGA','MUNI','LYON','VIEN','MILA']
@@ -18,12 +19,18 @@ pairs_ip[(3,6)]='.0.10.'
 pairs_ip[(4,8)]='.0.11.'
 pairs_ip[(4,7)]='.0.12.'
 
+subnets = defaultdict(lambda:set())
+
 # reverse for further query
 def init_pairs():
     cp = pairs_ip.copy()
     for p in cp:
         a, b = p
         pairs_ip[(a, b)] = GROUP_ID + pairs_ip[(a, b)]
+
+        subnets[a].add(pairs_ip[(a, b)] + '0/24')
+        subnets[b].add(pairs_ip[(a, b)] + '0/24')
+
         pairs_ip[(b, a)] = pairs_ip[(a, b)] + '2/24'
         pairs_ip[(a, b)] += '1/24'
 
@@ -40,6 +47,9 @@ def get_router_loopback(router_id):
     cmd = ''
     cmd += 'interface lo\n'
     cmd += 'ip address '+ GROUP_ID+'.%s.0.1/24\n'%(str(150 + router_id))
+
+    subnets[router_id].add(GROUP_ID+'.%s.0.0/24'%(str(150 + router_id)))
+
     return cmd
 
 def get_router_to_host_command(router_id):
@@ -49,9 +59,30 @@ def get_router_to_host_command(router_id):
     cmd += 'ip address '+ GROUP_ID + '.%s.0.2/24' %(str(router_id + 100))
     return cmd
 
+def get_router_ospf_command(router_id):
+    cmd = ''
+    cmd += 'conf t\nrouter ospf\n'
+    for s in subnets[router_id]:
+        cmd += 'network %s area 0\n' %(s)
+    return cmd
+
 init_pairs()
 router_id = int(sys.argv[1])
+print('************************************************')
+print('configure interface for router')
+print('************************************************')
 print('conf t')
 print(get_inter_router_command(router_id))
 print(get_router_loopback(router_id))
 print(get_router_to_host_command(router_id))
+print('exit\nexit\nexit\n')
+
+print('###########################################\nconfigure host###########################################\n'+'ip address add '+ GROUP_ID + '.%s.0.1/24' %(str(router_id + 100)) + ' dev %srouter' %(routers[router_id]))
+print('ip route add default via '+GROUP_ID + '.%s.0.2' %(str(router_id + 100)))
+print('exit\n')
+
+print('************************************************')
+print('configure ospf for router')
+print('************************************************')
+print(get_router_ospf_command(router_id))
+print('exit\nexit\nexit\n')
